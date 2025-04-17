f6208088bede21808f24747fb9911f70311c4b39
dcbiel846 <dcbiel846@gmail.com>
2025-04-17 01:12:16 +0000

    feat: added colors

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/ui/_core/app_colors.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      body: Column(
+        children: [
+          Image.asset('assets/banners/banner_splash.png'),
+          Image.asset('assets/logo.png', width: 192),
+          Text(
+            "um parceiro inovador para sua", 
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            "melhor experiencia culinaria", 
+            style: TextStyle(color: AppColors.mainColor),
+            ),
+          ElevatedButton(onPressed: () {}, child: Text("bora")),
+        ],
+      ),
+    );
   }
 }
